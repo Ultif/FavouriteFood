f12d1ff418933b33d289f4bf753793fd1b48c721
Ivan Kudriavtsev <kudrjavtsevvanek@gmail.com>
2022-11-24 02:29:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/favouriteFood/data/pojo/CategoryResponse.kt
@@ -0,0 +1,5 @@
+package com.example.favouriteFood.data.pojo
+
+data class CategoryResponse(
+    val categories: List<Category>
+)
---
+++ app/src/main/java/com/example/favouriteFood/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.favouriteFood.util
+
+class Constants {
+    companion object {
+        val MEAL_AREA = "MEAL_AREA"
+        val MEAL_ID = "MEAL_ID"
+        val MEAL_NAME = "MEAL_NAME"
+        val MEAL_STR = "MEAL_STR"
+        val MEAL_THUMB = "MEAL_THUMB"
+        val CATEGORY_NAME = "CATEGORY_NAME"
+    }
+}
